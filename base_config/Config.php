9de0ae2235b3e55223233d44f5c84c1cf5ce3725
raza11409652 <raza.11409652@lpu.in>
2020-07-31 15:15:14 +0530

    helo

--- base_config/Config.php
+++ base_config/Config.php
@@ -6,10 +6,17 @@ define('user' , 'root') ;
 define('password' ,'') ; 
 define('server' , 'localhost') ; 
 define('dbName' , 'hotel');
-
+define('RAZOR_PAY_API_KEY' , 'RAZOR KEY API');
+define('RAZOR_PAY_SECRET_TOKEN' , 'RAZOR PAY SECRET TOKEN');
 define('TOTAL_NUMBER_WORKFORCE' , 2);
-
-
+define('MSG_API_KEY' , 'apik key') ;
+define('OTP_TEMPLATE' , 'OTP template') ;
+/**Pay u money details */
+define('PAY_U_MERCH_KEY' , 'MERCH KEY');
+define('PAY_U_MERCH_SALT' , 'SALT');
+define('PAY_U_AUTH_HEADER' , 'HEADER+vmE=');
+define ("SECRETKEY", "SECRETKEYFOD");
+define("PAY_U_MONEY_URL" , "https://sandboxsecure.payu.in/_payment");
 
 
 
